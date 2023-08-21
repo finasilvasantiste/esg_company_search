a324224319353ca2e485f8814e8b7d6b850d368b
finasilvasantiste <fina.santiste@gmail.com>
2023-08-20 22:26:59 -0700

    test openai api

--- biz_logic/openai_handler.py
+++ biz_logic/openai_handler.py
@@ -4,3 +4,8 @@ import configparser
 config = configparser.ConfigParser()
 config.read_file((open(r'keys.cfg')))
 openai.api_key = config.get('OPENAI', 'ACCESS_KEY_ID')
+
+def test():
+	# list models
+	models = openai.Model.list()
+	print(models)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from biz_logic.pdf_handler import get_report_as_single_string
+from biz_logic.openai_handler import test
 
 def main():
     print("Hello World!")
@@ -6,4 +7,7 @@ def main():
 if __name__ == "__main__":
     company_name = 'airbnb' # Company name hardcoded during dev.
     report = get_report_as_single_string(company_name)
-    
+
+    # print(report)
+
+    test()
