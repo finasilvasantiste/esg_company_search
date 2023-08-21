c5a7c1b7c8543403dc294fd68ed2a5e1bacd744d
finasilvasantiste <fina.santiste@gmail.com>
2023-08-21 16:22:51 -0700

    add esg handler

---
+++ biz_logic/esg_handler.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class METRIC(Enum): 
+	""" 
+	Represents an ESG metric.
+	"""
+	CARBON_FOOTPRINT = "Carbon Footprint/GHG Emissions"
+	WATER_USAGE = "Water Usage/Water Management"
+	DIVERSITY_AND_INCLUSION = "Diversity and Inclusion"
+	EXECUTIVE_COMPENSATION = "Executive Compensation"
+	# Only a few metrics so start out with.
