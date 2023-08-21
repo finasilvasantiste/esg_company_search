6421ebde6c815785b39b3fd86982572589b6b451
finasilvasantiste <fina.santiste@gmail.com>
2023-08-20 17:43:14 -0700

    get report file path

--- pdf_handler.py
+++ pdf_handler.py
@@ -1,5 +1,9 @@
 from pypdf import PdfReader
+from enum import Enum
 
+class Company(Enum):
+	MICROSOFT = "esg_reports/microsoft_2022.pdf"
+	AIRBNB = "esg_reports/airbnb_2021.pdf"
 
 def test():
 	reader = PdfReader("esg_reports/microsoft_2022.pdf")
@@ -8,4 +12,17 @@ def test():
 	text = page.extract_text()
 
 	print(number_of_pages)
-	
+	print(text)
+
+
+def get_report_file_path(company_name):
+	"""
+	Returns report file path 
+	by given company name.
+	"""
+	if company_name.upper() in dir(Company):
+		file_path = Company[company_name.upper()].value
+	else:
+		file_path = None 
+
+	return file_path
