10871c3c403a8297430baee5ba456f5cba2dadbe
finasilvasantiste <fina.santiste@gmail.com>
2023-08-20 22:15:05 -0700

    add open ai api, add gitignore, add config

---
+++ biz_logic/openai_handler.py
@@ -0,0 +1,8 @@
+import openai
+import configparser
+
+# config = configparser.ConfigParser()
+# config.read_file((open(r'dwh.cfg')))
+# aws_user = self.aws_users['admin']
+
+# openai.api_key = config.get(openai, 'OPENAI_KEY_ID')
