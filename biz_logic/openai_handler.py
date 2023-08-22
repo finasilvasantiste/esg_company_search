8f48abf3bfd081fa51a61c0215bc65a3383ea6ad
finasilvasantiste <fina.santiste@gmail.com>
2023-08-22 12:50:18 -0700

    Split string to accomodate for token max

--- biz_logic/openai_handler.py
+++ biz_logic/openai_handler.py
@@ -2,39 +2,67 @@ import openai
 import configparser
 from biz_logic.esg_handler import Metric
 
-config = configparser.ConfigParser()
-config.read_file((open(r'keys.cfg')))
-openai.api_key = config.get('OPENAI', 'ACCESS_KEY_ID')
-
-def test():
-	# list models
-	models = openai.Model.list()
-	print(models)
+# config = configparser.ConfigParser()
+# config.read_file((open(r'keys.cfg')))
+# openai.api_key = config.get('OPENAI', 'ACCESS_KEY_ID')
 
 
-def question_example():
-	context = "Albert Einstein was a German-born theoretical physicist who developed the theory of relativity."
-	question = "Where was Albert Einstein born?"
-	response = openai.Completion.create(
-	  engine="text-davinci-003",
-	  prompt=f"Question answering:\nContext: {context}\nQuestion: {question}",
-	  max_tokens=50
-	)
-
-	answer = response.choices[0].text.strip()
-	print(answer)
-
-
-def ask_esg_questions(company_name, esg_report):
-	context = esg_report
-	metric = Metric.CARBON_FOOTPRINT.value
-	question = "What is {} doing about {}?".format(company_name, metric)
+class LLMBot():
+	config = configparser.ConfigParser()
+	config.read_file((open(r'keys.cfg')))
+	openai.api_key = config.get('OPENAI', 'ACCESS_KEY_ID')		
 	
-	response = openai.Completion.create(
-	  engine="text-davinci-003",
-	  prompt=f"Question answering:\nContext: {context}\nQuestion: {question}",
-	  max_tokens=200
-	)
+	def __init__(self, engine='text-davinci-003'):
+		self.engine = engine	
 
-	answer = response.choices[0].text.strip()
-	print(answer)	
+
+	def ask_questions(self, company_name, report):
+		context = report
+		metric = Metric.CARBON_FOOTPRINT.value
+		question = "What is {} doing about {}? Use the context provided.".format(company_name, metric)
+
+		response = openai.Completion.create(
+		  engine=self.engine,
+		  prompt=f"Question answering:\nContext: {context}\nQuestion: {question}",
+		  max_tokens=200
+		)
+
+		answer = response.choices[0].text.strip()
+		print(answer)	
+
+
+# def test():
+# 	# list models
+# 	models = openai.Model.list()
+# 	print(models)
+
+
+# def question_example():
+# 	context = "Albert Einstein was a German-born theoretical physicist who developed the theory of relativity."
+# 	question = "Where was Albert Einstein born?"
+# 	response = openai.Completion.create(
+# 	  engine="text-davinci-003",
+# 	  prompt=f"Question answering:\nContext: {context}\nQuestion: {question}",
+# 	  max_tokens=50
+# 	)
+
+# 	answer = response.choices[0].text.strip()
+# 	print(answer)
+
+
+# def ask_esg_questions(company_name, esg_report):
+# 	context = esg_report
+# 	metric = Metric.CARBON_FOOTPRINT.value
+# 	question = "What is {} doing about {}?".format(company_name, metric)
+# 	# engine="text-davinci-003" # limit of 4k tokens
+# 	engine="gpt-3.5-turbo-16k-0613"
+
+# 	response = openai.Completion.create(
+# 	  engine=engine,
+# 	  prompt=f"Question answering:\nContext: {context}\nQuestion: {question}",
+# 	  max_tokens=200
+# 	)
+
+# 	answer = response.choices[0].text.strip()
+# 	print(answer)	
+
--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from biz_logic.pdf_handler import get_report_as_single_string
-from biz_logic.openai_handler import test, question_example, ask_esg_questions
+from biz_logic.openai_handler import LLMBot
 
-def main():
-    print("Hello World!")
-
-if __name__ == "__main__":
+def main(engine):
     company_name = 'airbnb' # Company name hardcoded during dev.
     report = get_report_as_single_string(company_name)
 
-    # print(report)
+    engine = engine
+    bot = LLMBot(engine)
+    bot.ask_questions(company_name, report[0:4000]) # 4,097 token max for this engine
 
-    # test()
-    # question_example()
-    ask_esg_questions(company_name, report)
+    
+
+if __name__ == "__main__":
+    # company_name = 'airbnb' # Company name hardcoded during dev.
+    # report = get_report_as_single_string(company_name)
+
+    # engine = 'gpt-4-0613'
+    # bot = LLMBot(engine)
+    # bot.ask_questions(company_name, report)
+
+    print("Hello World!")
+
