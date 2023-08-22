b5d62490fc97ae3b9ce05fad8ea6f325aceac8c5
finasilvasantiste <fina.santiste@gmail.com>
2023-08-22 12:57:01 -0700

    cleanup

--- biz_logic/openai_handler.py
+++ biz_logic/openai_handler.py
@@ -2,10 +2,6 @@ import openai
 import configparser
 from biz_logic.esg_handler import Metric
 
-# config = configparser.ConfigParser()
-# config.read_file((open(r'keys.cfg')))
-# openai.api_key = config.get('OPENAI', 'ACCESS_KEY_ID')
-
 
 class LLMBot():
 	config = configparser.ConfigParser()
@@ -29,40 +25,4 @@ class LLMBot():
 
 		answer = response.choices[0].text.strip()
 		print(answer)	
-
-
-# def test():
-# 	# list models
-# 	models = openai.Model.list()
-# 	print(models)
-
-
-# def question_example():
-# 	context = "Albert Einstein was a German-born theoretical physicist who developed the theory of relativity."
-# 	question = "Where was Albert Einstein born?"
-# 	response = openai.Completion.create(
-# 	  engine="text-davinci-003",
-# 	  prompt=f"Question answering:\nContext: {context}\nQuestion: {question}",
-# 	  max_tokens=50
-# 	)
-
-# 	answer = response.choices[0].text.strip()
-# 	print(answer)
-
-
-# def ask_esg_questions(company_name, esg_report):
-# 	context = esg_report
-# 	metric = Metric.CARBON_FOOTPRINT.value
-# 	question = "What is {} doing about {}?".format(company_name, metric)
-# 	# engine="text-davinci-003" # limit of 4k tokens
-# 	engine="gpt-3.5-turbo-16k-0613"
-
-# 	response = openai.Completion.create(
-# 	  engine=engine,
-# 	  prompt=f"Question answering:\nContext: {context}\nQuestion: {question}",
-# 	  max_tokens=200
-# 	)
-
-# 	answer = response.choices[0].text.strip()
-# 	print(answer)	
 
--- main.py
+++ main.py
@@ -11,13 +11,5 @@ def main():
     
 
 if __name__ == "__main__":
-    # company_name = 'airbnb' # Company name hardcoded during dev.
-    # report = get_report_as_single_string(company_name)
-
-    # engine = 'gpt-4-0613'
-    # bot = LLMBot(engine)
-    # bot.ask_questions(company_name, report)
-
-    # print("Hello World!")
     main()
 
