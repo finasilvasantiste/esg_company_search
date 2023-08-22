330fed485896a98c906cc1787548e82fa1542943
finasilvasantiste <fina.santiste@gmail.com>
2023-08-22 12:56:15 -0700

    edit prompt

--- biz_logic/openai_handler.py
+++ biz_logic/openai_handler.py
@@ -12,19 +12,19 @@ class LLMBot():
 	config.read_file((open(r'keys.cfg')))
 	openai.api_key = config.get('OPENAI', 'ACCESS_KEY_ID')		
 	
-	def __init__(self, engine='text-davinci-003'):
-		self.engine = engine	
+	def __init__(self):
+		self.engine = 'text-davinci-003'	
 
 
 	def ask_questions(self, company_name, report):
 		context = report
 		metric = Metric.CARBON_FOOTPRINT.value
-		question = "What is {} doing about {}? Use the context provided.".format(company_name, metric)
+		question = "What is {} doing about {}? Use the context provided. Use quotation marks whenever you are quoting from the context.".format(company_name, metric)
 
 		response = openai.Completion.create(
 		  engine=self.engine,
 		  prompt=f"Question answering:\nContext: {context}\nQuestion: {question}",
-		  max_tokens=200
+		  max_tokens=300
 		)
 
 		answer = response.choices[0].text.strip()
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from biz_logic.pdf_handler import get_report_as_single_string
 from biz_logic.openai_handler import LLMBot
 
-def main(engine):
+def main():
     company_name = 'airbnb' # Company name hardcoded during dev.
     report = get_report_as_single_string(company_name)
 
-    engine = engine
-    bot = LLMBot(engine)
+    bot = LLMBot()
     bot.ask_questions(company_name, report[0:4000]) # 4,097 token max for this engine
 
     
@@ -19,5 +18,6 @@ if __name__ == "__main__":
     # bot = LLMBot(engine)
     # bot.ask_questions(company_name, report)
 
-    print("Hello World!")
+    # print("Hello World!")
+    main()
 
