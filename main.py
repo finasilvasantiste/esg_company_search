c106a1f87e5031311372e74fc462cfab18b71ca4
finasilvasantiste <fina.santiste@gmail.com>
2023-08-25 15:54:07 -0700

    comment out api calls to openai during dev

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ def main():
     first_report_half = report[0:4000] # 4,097 token max for LLM engine
 
     bot = LLMBot(company = company_name, report = first_report_half)
-    bot.ask_question(topic = ESGMetric.CARBON_FOOTPRINT.value)
-    bot.ask_question(topic = ESGMetric.WATER_USAGE.value)
-    bot.ask_question(topic = ESGMetric.DIVERSITY_AND_INCLUSION.value)
-    bot.ask_question(topic = ESGMetric.EXECUTIVE_COMPENSATION.value)
+
+    # Commenting out the api calls to openai during dev.
+    # bot.ask_question(topic = ESGMetric.CARBON_FOOTPRINT.value)
+    # bot.ask_question(topic = ESGMetric.WATER_USAGE.value)
+    # bot.ask_question(topic = ESGMetric.DIVERSITY_AND_INCLUSION.value)
+    # bot.ask_question(topic = ESGMetric.EXECUTIVE_COMPENSATION.value)
     
 
 if __name__ == "__main__":
