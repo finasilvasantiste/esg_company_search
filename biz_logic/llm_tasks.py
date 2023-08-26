6fe40878304cfb27b05f860a4d6772b742894539
finasilvasantiste <fina.santiste@gmail.com>
2023-08-26 14:13:43 -0700

    debug task flow issue

---
+++ biz_logic/llm_tasks.py
@@ -0,0 +1,23 @@
+from prefect import task
+
+
+@task
+def llm_ask_esg_questions(company, report):
+	""" 
+	Asks LLM bot ESG questions using given
+	company name and report.
+
+	Parameters:
+		company_name (str): company name
+		report (str): company report
+	Returns:
+		None
+	"""
+	first_report_half = report[0:4000] # 4,097 token max for LLM engine
+    bot = LLMBot(company = company_name, report = first_report_half)
+
+    # Commenting out the api calls to openai during dev.
+    # bot.ask_question(topic = ESGMetric.CARBON_FOOTPRINT.value)
+    # bot.ask_question(topic = ESGMetric.WATER_USAGE.value)
+    # bot.ask_question(topic = ESGMetric.DIVERSITY_AND_INCLUSION.value)
+    # bot.ask_question(topic = ESGMetric.EXECUTIVE_COMPENSATION.value)
--- biz_logic/search_flow.py
+++ biz_logic/search_flow.py
@@ -5,16 +5,16 @@ from biz_logic.pdf_tasks import get_report_as_single_string
 
 @flow
 def esg_company_search(company_name):
+    """ 
+    Workflow which runs an ESG company search.
+    """
     print('Hello flow!')
     report = get_report_as_single_string(company_name)
-    first_report_half = report[0:4000] # 4,097 token max for LLM engine
+    # first_report_half = report[0:4000] # 4,097 token max for LLM engine
 
-    bot = LLMBot(company = company_name, report = first_report_half)
-
-    # Commenting out the api calls to openai during dev.
-    # bot.ask_question(topic = ESGMetric.CARBON_FOOTPRINT.value)
-    # bot.ask_question(topic = ESGMetric.WATER_USAGE.value)
-    # bot.ask_question(topic = ESGMetric.DIVERSITY_AND_INCLUSION.value)
-    # bot.ask_question(topic = ESGMetric.EXECUTIVE_COMPENSATION.value)
+    # bot = LLMBot(company = company_name, report = first_report_half)
     
     print('Bye flow!')
+
+if __name__ == "__main__":
+    esg_company_search('airbnb')
