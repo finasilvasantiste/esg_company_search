90d581d691c990c101e8ca814109a73baa9668bf
finasilvasantiste <fina.santiste@gmail.com>
2023-08-28 18:00:21 -0700

    fix indent issue

--- biz_logic/llm_tasks.py
+++ biz_logic/llm_tasks.py
@@ -20,15 +20,15 @@ def llm_ask_esg_questions(company, report):
 	# print(first_report_half)
 
 	first_report_half = LLMBot.cut_string_to_max_token_size(report)
-	print(first_report_half)
-	print('DONE')
+	# print(first_report_half)
+	# print('DONE')
 	# first_report_half = report[0:4000] # 4,097 token max for LLM engine
 	# print(first_report_half)
-	# bot = LLMBot(company = company_name, report = first_report_half)
+	bot = LLMBot(company = company, report = first_report_half)
 
 	# print('ABOUT TO ASK QUESTION')
 	# Commenting out the api calls to openai during dev.
-	# bot.ask_question(topic = ESGMetric.CARBON_FOOTPRINT.value)
+	bot.ask_question(topic = ESGMetric.CARBON_FOOTPRINT.value)
     # bot.ask_question(topic = ESGMetric.WATER_USAGE.value)
     # bot.ask_question(topic = ESGMetric.DIVERSITY_AND_INCLUSION.value)
     # bot.ask_question(topic = ESGMetric.EXECUTIVE_COMPENSATION.value)
