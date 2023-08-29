04ff1cd22a991204601157451334709a5efd62a0
finasilvasantiste <fina.santiste@gmail.com>
2023-08-29 16:04:23 -0700

    cleanup comment

--- biz_logic/llm_tasks.py
+++ biz_logic/llm_tasks.py
@@ -17,7 +17,6 @@ def llm_ask_esg_questions(company, report):
 	first_report_half = LLMBot.cut_string_to_max_token_size(report)
 	bot = LLMBot(company = company, report = first_report_half)
 
-	# print('ABOUT TO ASK QUESTION')
 	# Commenting out the api calls to openai during dev.
 	# bot.ask_question(topic = ESGMetric.CARBON_FOOTPRINT.value)
     # bot.ask_question(topic = ESGMetric.WATER_USAGE.value)
