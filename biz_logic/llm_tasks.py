c0b044dafa655ac370ca5fd65db89d4f7b8a368b
finasilvasantiste <fina.santiste@gmail.com>
2023-08-28 18:01:52 -0700

    cleanup

--- biz_logic/llm_tasks.py
+++ biz_logic/llm_tasks.py
@@ -14,16 +14,7 @@ def llm_ask_esg_questions(company, report):
 	Returns:
 		None
 	"""
-	print('INSIDE TASK!')
-	# print(report[0:4000])
-	# first_report_half = report[0:4000]
-	# print(first_report_half)
-
 	first_report_half = LLMBot.cut_string_to_max_token_size(report)
-	# print(first_report_half)
-	# print('DONE')
-	# first_report_half = report[0:4000] # 4,097 token max for LLM engine
-	# print(first_report_half)
 	bot = LLMBot(company = company, report = first_report_half)
 
 	# print('ABOUT TO ASK QUESTION')
@@ -32,5 +23,3 @@ def llm_ask_esg_questions(company, report):
     # bot.ask_question(topic = ESGMetric.WATER_USAGE.value)
     # bot.ask_question(topic = ESGMetric.DIVERSITY_AND_INCLUSION.value)
     # bot.ask_question(topic = ESGMetric.EXECUTIVE_COMPENSATION.value)
-
-    # print('TASK DONE')
